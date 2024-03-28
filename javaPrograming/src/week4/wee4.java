af8ae8909c75628eb8b35c7e41b8e476234d7b2f
admin <admin@DESKTOP-121PAKN>
2024-03-28 12:58:32 +0900

    week4

---
+++ javaPrograming/src/week4/wee4.java
@@ -0,0 +1,28 @@
+package week4;
+
+public class wee4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		System.out.println(69/10);
+//		System.out.println(69%10);
+		int n = 5;
+//		int r = n % 2;
+//		System.out.println("r : " + r);
+		
+		int r = n % 2;
+		
+		int i = 1;
+		i = i*3;
+		int j = i;
+		
+		
+		
+		System.out.println("i : " + i);
+		System.out.println("J : " + j);
+	
+	
+	
+	}
+
+}
