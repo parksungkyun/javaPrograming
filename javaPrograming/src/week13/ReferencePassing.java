2bd2074cb4630008957865fda249897351bf8de4
admin <admin@DESKTOP-121PAKN>
2024-06-14 12:53:51 +0900

    hhhh

---
+++ javaPrograming/src/week13/ReferencePassing.java
@@ -0,0 +1,26 @@
+package week13;
+class Circles {
+	int radius;
+
+	public Circles(int radius) {
+		this.radius = radius;
+	}
+
+}
+
+public class ReferencePassing {
+
+	static void increase(Circles m) {
+		m.radius++;
+	}
+
+	public static void main(String args[]) {
+		Circles pizza = new Circles(10);
+
+		increase(pizza);
+
+		System.out.println(pizza.radius);
+
+	}
+
+}
